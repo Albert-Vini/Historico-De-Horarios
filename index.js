6d21e7ead154cb17a4ea24c67f74f3c945fa563b
a114275 <a114275@alunos.uminho.pt>
2026-08-30 21:07:19 +0100

    Adiciona os codigos javascript pra localização
    Adiciona os codigos pra salvar a localização da pessoa, posteriomente será usado esses dados

--- index.js
+++ index.js
@@ -3,66 +3,153 @@ const chaveActivate = "KeyBool";
 const versaoData = "DataVersion";
 const modo = "modoLight";
 
+const estiloData = "ElementosVisuais";
+const arraysData = "dadosHeL";
+
+//Cria um objeto com as informações de arrays
+function arrayDataC(version, hArray, lArray){
+    this.version = version;
+    this.horarios = hArray;
+    this.locations = lArray;
+}
+
+//Cria um objeto com os dados de estilo
+function estiloDataC(light){
+    this.modoLight = light;
+}
+
+///Abre as informações da pagina com base nas versão 1
+function openHistoryV1() {
+    let array = JSON.parse(localStorage.getItem(chaveArray)) || [];
+    let boolean = JSON.parse(localStorage.getItem(chaveActivate)) || false;
+    let version = JSON.parse(localStorage.getItem(versaoData)) || 1;
+    let light = JSON.parse(localStorage.getItem(modo)) || 0;
+    return { array, boolean, version, light };
+}
+
+//Função para atualizar os dados da versão 1 para 2
+function update1to2(){
+    const dadosSalvos = openHistoryV1();
+
+    //Salva os elementos visuais em objetos
+    let elementosVisuais = new estiloDataC(dadosSalvos.light);
+    localStorage.removeItem("KeyBool");
+    localStorage.removeItem("modoLight");
+
+    //Salva os dados de horario e localização
+    let dadosHeL = new arrayDataC(2,dadosSalvos.array,[]);
+    (dadosHeL.horarios).forEach((array) => {
+        if(array.length == 2)dadosHeL.locations.push([null,null])
+        else if(array.length == 1) dadosHeL.locations.push([null])
+    });
+    localStorage.removeItem("HorariosSave");
+    localStorage.removeItem("DataVersion");
+    localStorage.setItem(estiloData,JSON.stringify(elementosVisuais));
+    localStorage.setItem(arraysData,JSON.stringify(dadosHeL));
+}
+if(JSON.parse(localStorage.getItem(versaoData)) == 1) update1to2();
+
+
 //---------------------------Codigos de gerenciamento do historico de horario
 ///limpa o historico de senhas
-function clearHistory() {
-    localStorage.setItem(chaveArray, JSON.stringify([]));
-    localStorage.setItem(chaveActivate, JSON.stringify(false));
-    localStorage.setItem(versaoData, JSON.stringify(1));
+function clearArrays() {
+    localStorage.removeItem(arraysData);
+    location.reload();
+}
+
+function resetStyleData(){
+    localStorage.removeItem(estiloData);
     location.reload();
 }
 
 ///Abre as informações da pagina
-function openHistory() {
-    array = JSON.parse(localStorage.getItem(chaveArray)) || [];
-    boolean = JSON.parse(localStorage.getItem(chaveActivate)) || false;
-    version = JSON.parse(localStorage.getItem(versaoData)) || 1;
-    light = JSON.parse(localStorage.getItem(modo)) || 0;
-    return { array, boolean, version, light };
+function openArrayData() {
+    let dados = JSON.parse(localStorage.getItem(arraysData));
+    if (!dados) return new arrayDataC(2, [], []);
+    if (!dados.horarios) dados.horarios = [];
+    if (!dados.locations) dados.locations = [];
+    
+    return dados;
 }
 
-///Salva o historico de horarios
-function saveHistory(array, boolean, version, light) {
-    localStorage.setItem(chaveArray, JSON.stringify(array));
-    localStorage.setItem(chaveActivate, JSON.stringify(boolean));
-    localStorage.setItem(versaoData, JSON.stringify(version));
-    localStorage.setItem(modo, JSON.stringify(light));
+function openStyleData(){
+    return JSON.parse(localStorage.getItem(estiloData)) || new estiloDataC(0);
+}
+
+function saveArraysData(dataHeL){
+    localStorage.setItem(arraysData, JSON.stringify(dataHeL));
+}
+
+function saveStyleData(styleDataNew){
+    localStorage.setItem(estiloData, JSON.stringify(styleDataNew));
 }
 
 ///Verifica os horarios
-function seeHistory() {
-    console.log(JSON.parse(localStorage.getItem(chaveArray)));
+function seeArraysData() {
+    console.log(JSON.parse(localStorage.getItem(arraysData)));
 }
 
 //verifica a versão dos dados salvos
 function seeVersion() {
-    console.log(String(JSON.parse(localStorage.getItem(versaoData))));
+    console.log(JSON.parse(localStorage.getItem(arraysData)).version);
 }
 //------------------------------------------------------------------------------
 
-//Funcoes relacionadas com o horario o salvamento na lista
+//Funcoes relacionadas com o horario e localização
 //Devolve um array com o horario atual
 function tempo() {
     const time = new Date();
     return [time.getFullYear(), time.getMonth(), time.getDate(), time.getHours(), time.getMinutes(), time.getSeconds()];
 }
 
-//Adiciona no array o tempo atual
-function saveTime() {
-    let { array: horarios, boolean: activate, version , light } = openHistory();
-    if (!activate) {
-        horarios.push([tempo()]);
-    }
-    else {
-        horarios[horarios.length - 1].push(tempo());
-    }
-    activate = !activate;
-    saveHistory(horarios, activate, version, light);
-    return activate;
+//Devolve um array com a localização atual e a precisão ou 0 em caso de erro
+function position() {
+    return new Promise((resolve) => {
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(
+                (posicao) => {
+                    let latitude = posicao.coords.latitude;
+                    let longitude = posicao.coords.longitude;
+                    let precisao = posicao.coords.accuracy;
+                    resolve([latitude, longitude, precisao]);
+                },
+                () => resolve(0),
+                { timeout: 5000 }
+            );
+        } else {
+            resolve(0);
+        }
+    });
 }
 
-function tempoDecorrido(intervalo) {
-    let [inicio, fim] = intervalo
+//Devolve se o botão de play ta ativado
+function activate(){
+    let horarios = openArrayData().horarios;
+    if(horarios.length == 0 || horarios[horarios.length - 1].length == 2) return 0;
+    return 1;
+}
+
+//Adiciona nos dados de arrays o tempo atual e a localização
+async function saveTime() {
+    let arrays = openArrayData();
+    let posicao = await position();
+    let isActivate = activate();
+    if(posicao == 0) posicao = undefined;
+    if (!isActivate) {
+        arrays.horarios.push([tempo()]);
+        arrays.locations.push([posicao]);
+    }
+    else {
+        let lengthA = arrays.horarios.length - 1
+        arrays.horarios[lengthA].push(tempo());
+        arrays.locations[lengthA].push(posicao)
+    }
+    saveArraysData(arrays);
+    return isActivate;
+}
+
+//Calcula o intervalo entre dois tempos
+function tempoDecorrido(inicio, fim) {
     if(!fim)return{horas:0, min:0, seg:0};
     let data1 = new Date(...inicio);
     let data2 = new Date(...fim);
@@ -76,11 +163,11 @@ function tempoDecorrido(intervalo) {
 //------------------------------------------------------------------------------
 
 //Atualiza o botão pra o activate atual
-function updateButton(activate) {
+function updateButton() {
+    const isactivate = activate();
     const botao = document.getElementById("botao");
     const icone = document.getElementById("icone");
-
-    if (activate) {
+    if (isactivate) {
         botao.className = "botao-stop";
         icone.className = "stop-icon";
     }
@@ -90,7 +177,7 @@ function updateButton(activate) {
     }
 }
 
-// Retorna, se for um numero, uma string de tamanho 2 ou mais
+//Se for um numero, Retorna uma string de tamanho 2 ou mais
 function fN(Num){
     return isNaN(Num) ? Num : (String(Num).padStart(2,"0"));
 }
@@ -100,19 +187,20 @@ function mudarEstilo(estilo, variaveis){
     Object.entries(variaveis).forEach(([variavel, valor]) => estilo.setProperty(variavel,valor));
 }
 
+//muda o tema se a pagina tiver um botão pra tal
 if(document.getElementById("MudarTema"))
 {
-    if(openHistory().light =="1") definirTema(1);
+    if(openStyleData().modoLight == 1) definirTema(1);
     else definirTema(0);
     const botao = document.getElementById("MudarTema");
     botao.onclick = function() 
     {
-        let light = openHistory().light;
-        Number(light);
-        definirTema(!Number(light));
+        let light = openStyleData().modoLight;
+        definirTema(!light);
     }
 }
 
+//define o tema de uma pagina
 function definirTema(light)
 {
     const botao = document.getElementById("MudarTema");
@@ -145,60 +233,61 @@ function definirTema(light)
         mudarEstilo(styleCha,black);
         botao.textContent = "Mudar tema 🌒";
     }
-    localStorage.setItem(modo, JSON.stringify(light));
+    saveStyleData(new estiloDataC(light));
 }
 //-------------------------------Funcoes Especiais para cada pagina
 
 //Funcoes da pagina Principal
 if (document.getElementById("principal")) {
     let botao = document.getElementById("botao");
-    let dataHistory = openHistory()
-    updateButton(dataHistory.boolean);
-    botao.onclick = function () {
-        let activate = saveTime();
-        updateButton(activate);
+    let arrays = openArrayData();
+    updateButton();
+    botao.onclick = async function () {
+        await saveTime();
+        updateButton();
     }
+}
+
+//Cria uma tabela com os horarios
+function creatTable(array) {
+    const tBody = document.getElementById("TabelaHorario");
+    array.forEach((element,indice) => {
+        let novalinha = document.createElement("tr");
+        let {horas, min, seg} = tempoDecorrido(element[0],element[1]);
+        let minFormatado = fN(min);
+        let horaFormatada = fN(horas);
+        let segFormatado = fN(seg);
+        let saida = element[1] || ["--", "--", "--", "--", "--", "--"];
+        novalinha.innerHTML = 
+        `
+        <td>${indice+1}</td>
+        <td>
+            <div class="celulaDividida">
+                <div>${fN(element[0][2])}/${fN(element[0][1]+1)}/${fN(element[0][0])}</div>
+                <div>${fN(saida[2])}/${ saida[1] != "--" ? fN(saida[1] +1) : saida[1]}/${fN(saida[0])}</div>
+            </div>
+        </td>
+        <td>${horaFormatada}:${minFormatado}:${segFormatado}</td>
+        <td>
+            <div class="celulaDividida">
+                <div>${fN(element[0][3])}:${fN(element[0][4])}:${fN(element[0][5])}</div>
+                <div>${fN(saida[3])}:${fN(saida[4])}:${fN(saida[5])}</div>
+            </div>
+        </td>
+        `;
+        tBody.appendChild(novalinha);
+    });
 }
 
 //Funcoes da pagina da tabela
 if(document.getElementById("tabela")){
     const clearButton = document.getElementById("clearHistory");
-    let {array} = openHistory();
+    let array = openArrayData().horarios;
     creatTable(array);
-    function creatTable(array) {
-        const tBody = document.getElementById("TabelaHorario");
-        array.forEach((element,indice) => {
-            let novalinha = document.createElement("tr");
-            let {horas, min, seg} = tempoDecorrido(element);
-            let minFormatado = fN(min);
-            let horaFormatada = fN(horas);
-            let segFormatado = fN(seg);
-            let saida = element[1] || ["--", "--", "--", "--", "--", "--"];
-            novalinha.innerHTML = 
-            `
-            <td>${indice+1}</td>
-            <td>
-                <div class="celulaDividida">
-                    <div>${fN(element[0][2])}/${fN(element[0][1]+1)}/${fN(element[0][0])}</div>
-                    <div>${fN(saida[2])}/${ saida[1] != "--" ? fN(saida[1] +1) : saida[1]}/${fN(saida[0])}</div>
-                </div>
-            </td>
-            <td>${horaFormatada}:${minFormatado}:${segFormatado}</td>
-            <td>
-                <div class="celulaDividida">
-                    <div>${fN(element[0][3])}:${fN(element[0][4])}:${fN(element[0][5])}</div>
-                    <div>${fN(saida[3])}:${fN(saida[4])}:${fN(saida[5])}</div>
-                </div>
-            </td>
-            `;
-            tBody.appendChild(novalinha);
-        });
-    }
-
     clearButton.onclick = function(){
         let senha = window.prompt("Confirme a limpeza digitando `Sim` da mesma forma:");
         if(senha == "Sim"){
-            clearHistory();
+            clearArrays();
         }
     }
 }
